63740aecbb1f8fe18c9cafed20bd0ae23916fd1e
Snezhana Petlova <snpetlova@gmail.com>
2023-10-10 20:53:25 +0200

    Cubicle

--- Cubicle/src/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-const PORT = 5050;
-
-app.get('/', (req, res) => {
-    res.send('helloooo')
-});
-
-app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}...`));
---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+
+const app = express();
+const PORT = 8000;
+
+//Handlebars Configuration
+app.engine("hbs", handlebars.engine({ extname: "hbs" }));
+app.set("view engine", "hbs");
+app.set("views", "./src/views")
+
+
+app.get("/", (req, res) => {
+   res.render("index");
+});
+
+app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}...`));
