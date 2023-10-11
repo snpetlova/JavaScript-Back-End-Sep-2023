4ded92d2772ba03b106f40727591a21f10c40590
Snezhana Petlova <snpetlova@gmail.com>
2023-10-11 23:18:21 +0200

    Cubicle

--- ExpressJS-and-Templating/src/controllers/homeController.js
+++ ExpressJS-and-Templating/src/controllers/homeController.js
@@ -1,20 +1,23 @@
 const router = require('express').Router();
+const cubeService = require('../services/cubeService')
 
 // const homeController = (req, res) => {
 //     res.render("index");
 // }
 
-router.get('/', (req, res) => {
-    res.render('index')
-});
+router.get("/", (req, res) => {
+    const { search, from, to } = req.query;
+    const cubes = cubeService.getAll(search, from, to);
+    res.render("index", { cubes, search, from, to });
+  });
 
 router.get('/about', (req, res) => {
     res.render('about')
 });
 
-router.get('*', (req, res) => {
-    res.render('404');
-});
+router.get('404', (req, res) => {
+    res.render('404')
+})
 
 module.exports = router;
 // module.exports = homeController;
--- ExpressJS-and-Templating/src/router.js
+++ ExpressJS-and-Templating/src/router.js
@@ -5,4 +5,8 @@ const cubeController = require('./controllers/cubeController');
 router.use(homeController);
 router.use('/cubes', cubeController);
 
+router.get('*', (req, res) => {
+    res.redirect('/404');
+});
+
 module.exports = router;
