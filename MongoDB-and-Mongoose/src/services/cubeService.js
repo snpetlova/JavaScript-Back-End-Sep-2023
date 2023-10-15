4e8ff375251e390c40fb5c6d53549a438f3aaa31
Snezhana Petlova <snpetlova@gmail.com>
2023-10-15 09:48:13 +0200

    details page, getSingleCube, getAll

--- MongoDB-and-Mongoose/src/services/cubeService.js
+++ MongoDB-and-Mongoose/src/services/cubeService.js
@@ -1,4 +1,4 @@
-const Cube = require('./../models/Cube');
+const Cube = require("./../models/Cube");
 const cubes = [];
 
 exports.create = async (cubeData) => {
@@ -9,9 +9,9 @@ exports.create = async (cubeData) => {
   return cube;
 };
 
-exports.getAll = (search, from, to) => {
-  let filterCubes = [...cubes];
-
+exports.getAll = async (search, from, to) => {
+  let filterCubes = await Cube.find().lean();
+ 
   if (search) {
     filterCubes = filterCubes.filter((cube) =>
       cube.name.toLowerCase().includes(search.toLowerCase())
@@ -33,6 +33,4 @@ exports.getAll = (search, from, to) => {
   return filterCubes;
 };
 
-exports.getSingleCube = (id) => {
-  return cubes.find((cube) => cube.id === id);
-};
+exports.getSingleCube = (id) => Cube.findById(id);
