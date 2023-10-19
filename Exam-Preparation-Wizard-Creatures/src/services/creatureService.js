6263dc2fe78cb5fe679ad76057139bbc5ed92ba9
Snezhana Petlova <snpetlova@gmail.com>
2023-10-19 11:58:40 +0200

    Exam Preparation
    delete functionality

--- Exam-Preparation-Wizard-Creatures/src/services/creatureService.js
+++ Exam-Preparation-Wizard-Creatures/src/services/creatureService.js
@@ -7,3 +7,5 @@ exports.getAll = () => Creature.find();
 exports.singleCreature = (creatureId) => Creature.findById(creatureId);
 
 exports.update = (creatureId, creatureData) => Creature.findByIdAndUpdate(creatureId, creatureData);
+
+exports.delete = (creatureId) => Creature.findByIdAndDelete(creatureId);
